1c874b915e4a9ff667aaf39fd7d702929525b268
pratap86 <narayanpratap86@gmail.com>
2020-12-28 21:43:30 +0530

    junit-with-mockito gradle app initial commit

---
+++ src/main/java/com/pratap/junit/JunitWithMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.pratap.junit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitWithMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitWithMockitoApplication.class, args);
+	}
+
+}
