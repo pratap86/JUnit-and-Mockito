33e8a53df95f53294d008c2d851aad7685cb1cf6
pratap86 <narayanpratap86@gmail.com>
2021-02-25 23:14:03 +0530

    mockito3 feature

---
+++ happyHotelApp/src/main/java/com/pratap/hotel/booking/exceptions/BusinessException.java
@@ -0,0 +1,7 @@
+package com.pratap.hotel.booking.exceptions;
+
+public class BusinessException extends RuntimeException {
+
+	private static final long serialVersionUID = -2842617807944569411L;
+
+}
